dadbe8502011bce1f0295630585466ee946c4ed4
Sandeep Yerramsetti <yerramsetti.sandeep15@gmail.com>
2018-08-26 11:25:46 -0400

    master commit

---
+++ src/components/posts_new.js
@@ -0,0 +1,79 @@
+import React, { Component, PropTypes } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
+
+class PostsNew extends Component {
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+    return (
+      <div className = {className}>
+        <label> {field.label} </label>
+        <input className="form-control" type="text" {...field.input} />
+        <div className="text-help">
+          {touched ? error : ''}
+
+        </div>
+      </div>
+    );
+
+  }
+
+  onSubmit(values) {
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+    <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+      <Field
+        label= "title"
+        name="title"
+        component = {this.renderField}
+      />
+      <Field
+        label= "categories"
+        name="categories"
+        component = {this.renderField}
+      />
+      <Field
+        label= "Post Content"
+        name="Content"
+        component = {this.renderField}
+      />
+      <button type="submit" className="btn btn-primary">Submit</button>
+      <Link className= "btn btn-danger" to = "/">
+      Cancel
+      </Link>
+    </form>
+    );
+  }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a title';
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter a categories';
+  }
+
+  if (!values.Content) {
+    errors.Content = 'Enter a Content';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm',
+})(connect(null, { createPost })(PostsNew));
